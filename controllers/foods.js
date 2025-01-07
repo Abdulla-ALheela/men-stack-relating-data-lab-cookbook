cc22547082e3f07a3fcd16a83a62c8a0091dea6a
Abdulla Alheela <Abdulla.alheela@gmail.com>
2025-01-07 16:31:52 +0300

    added a form to create new iteam

--- controllers/foods.js
+++ controllers/foods.js
@@ -3,6 +3,30 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-// router logic will go here - will be built later on in the lab
+router.get('/', (req, res) => {
+    res.render('foods/index.ejs');
+});
+
+router.get('/new', (req, res) => {
+    res.render('foods/new.ejs');
+});
+
+router.post('/', async (req, res) => {
+    try {
+      // Look up the user from req.session
+      const currentUser = await User.findById(req.session.user._id);
+      // Push req.body (the new form data object) to the
+      // food array of the current user
+      currentUser.pantry.push(req.body);
+      // Save changes to the user
+      await currentUser.save();
+      // Redirect back to the foods index view
+      res.redirect(`/users/${currentUser._id}/foods`);
+    } catch (error) {
+      // If any errors, log them and redirect back home
+      console.log(error);
+      res.redirect('/');
+    }
+  });
 
 module.exports = router;
